0e4d59b62b109e4b87500c5daf1a25cce3fc0164
Jelena Jovanovic <jelena.jjo@gmail.com>
2014-09-23 17:49:29 +0200

    23.09.14

---
+++ package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  summary: "Some simple and creative overlay styles and effects.",
+  version: "1.0.0",
+  name: "jelena:meteor-fullscreen-overlay",
+  git: "https://github.com/jelenajjo/meteor-fullscreen-overlay.git"
+});
+
+
+Package.onUse(function (api) {
+  api.addFiles('css/style1.css', 'client');
+  api.addFiles('css/style2.css', 'client');
+  api.addFiles('css/style3.css', 'client');
+  api.addFiles('css/style5.css', 'client');
+  api.addFiles('css/style6.css', 'client');
+  api.addFiles('css/style7.css', 'client');
+  api.addFiles('css/style8.css', 'client');
+  api.addFiles('css/style9.css', 'client');
+  api.addFiles('css/style10.css', 'client');
+  api.addFiles('css/style11.css', 'client');
+  api.addFiles('css/style12.css', 'client');
+  api.addFiles('js/modernizr.custom.js', 'client');
+  api.addFiles('js/classie.js', 'client');
+  api.addFiles('js/demo1.js', 'client');
+  api.addFiles('js/demo7.js', 'client');
+  api.addFiles('js/demo9.js', 'client');
+  api.addFiles('js/demo10.js', 'client');
+  api.addFiles('js/demo12.js', 'client');
+});
+
